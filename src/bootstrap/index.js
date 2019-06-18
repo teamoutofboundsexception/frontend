a3b523e36c31f4ebd54d966e2755aaa4eaff618d
ventaquil <konrad.golawski@gmail.com>
2019-06-18 23:24:52 +0200

    Add Vue FontsAwesome

--- src/bootstrap/index.js
+++ src/bootstrap/index.js
@@ -2,5 +2,8 @@ import Vue from 'vue'
 import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 Vue.use(BootstrapVue)
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
