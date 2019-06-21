d8253fe0c6bdd051091ffb1bd51273874f64597a
ventaquil <konrad.golawski@gmail.com>
2019-06-21 22:28:15 +0200

    Set default API endpoint URL

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,4 +1,9 @@
 'use strict'
+
+if (!process.env.API_ENDPOINT) {
+  process.env.API_ENDPOINT = 'http://localhost:8080/'
+}
+
 module.exports = {
   API_ENDPOINT: '"' + process.env.API_ENDPOINT + '"',
   NODE_ENV: '"production"'
