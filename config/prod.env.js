dd8788ad171dd601f35300f06eacf47339f1af5f
ventaquil <konrad.golawski@gmail.com>
2019-06-21 22:32:27 +0200

    Default API endpoint URL fix

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,7 +1,7 @@
 'use strict'
 
 if (!process.env.API_ENDPOINT) {
-  process.env.API_ENDPOINT = 'http://localhost:8080/'
+  process.env.API_ENDPOINT = 'http://localhost:8080'
 }
 
 module.exports = {
